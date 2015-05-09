6eb22ba318132966b7ba8cf4d69b68ce309dc3ac
CryptoStock <dahlbergra@mymail.vcu.edu>
2015-05-09 06:14:34 -0400

    Update Seed Node
    Update Seed Node

--- src/net.cpp
+++ src/net.cpp
@@ -1232,7 +1232,7 @@ void MapPort()
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strDNSSeed[][2] = {
-    {"seed.clonecoin.ninja", "seed.clonecoin.ninja"},
+    {NULL, NULL}
 
 };
 
